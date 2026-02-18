e84b69c8302d20b8a04ad6e24835768ebe630d9c
Mike Hamilton <mike.hamilton7@gmail.com>
2026-02-18 00:06:08 -0700

    Modernize packaging with pyproject, uv docs, and CI matrix

--- setup.py
+++ setup.py
@@ -1,62 +1,10 @@
 #!/usr/bin/env python
+"""Legacy setup shim.
+
+Prefer PEP 517/518 builds via `pyproject.toml`.
 """
-Distribution script for DinrSR, a program to identify
-differntial intron retention from short read data.
 
-\author Mike Hamilton
-"""
-from distutils.core import setup
-import sys
-SCRIPTS  = ['scripts/idiffir.py',
-            'scripts/getDepths.py',
-            'scripts/convertSam.py',
-            'scripts/simulate_IR.py',
-            'scripts/make_MISO_IR_GFF.py',
-            'scripts/make_MISO_AS_GFF.py',
-            'scripts/get_gene_expression.py',
-            'scripts/idiffir_plotter.py',
-            'scripts/run_miso_ir.py'            
-]
+from setuptools import setup
 
-PACKAGES = [ 'iDiffIR',
-             'iDiffIR.SpliceGrapher',
-             'iDiffIR.SpliceGrapher.formats',
-             'iDiffIR.SpliceGrapher.plot',
-             'iDiffIR.SpliceGrapher.predict',
-             'iDiffIR.SpliceGrapher.shared',
-             'iDiffIR.SpliceGrapher.statistics',
-             'iDiffIR.SpliceGrapher.view',
-]
-
-
-REQUIRES = ['scipy',
-            'numpy',
-            'matplotlib',
-            'pysam'
-]
-
-# check for dependencies if we're building or installing
-installable = True
-if 'build' in sys.argv or 'install' in sys.argv:
-    sys.stderr.write('Checking for dependencies...\n')
-    for package in REQUIRES:
-        try:
-            p = __import__( package )
-            sys.stderr.write('package "%s" found (version %s)\n' % (package, p.__version__))
-        except ImportError:
-            installable = False
-            sys.stderr.write('\tRequired package `%s` not found\n' % package )
-    if not installable:
-        sys.exit('Missing dependencies--terminating installation')
-
-setup(name='iDiffIR',
-      version='0.3.1',
-      description='Identifying differential intron retention from RNA-seq',
-      author='Michael Hamilton',
-      author_email='mike.hamilton7@gmail.com',
-      url='combi.cs.colostate.edu/idiffir',
-      packages=PACKAGES,
-      requires=REQUIRES,
-      scripts=SCRIPTS,
-      license='GNU General Public License'
-     )
+if __name__ == "__main__":
+    setup()
---
+++ tests/test_import.py
@@ -0,0 +1,5 @@
+import iDiffIR
+
+
+def test_version_present():
+    assert hasattr(iDiffIR, "__version__")
